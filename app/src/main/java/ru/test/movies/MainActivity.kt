36d10f4536d145595c505614ddc853e3c567a2d7
milkfist <milkfist@student.21-school.ru>
2023-09-15 11:50:27 +0300

    first commit

---
+++ app/src/main/java/ru/test/movies/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.test.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.test.movies.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
